a3f57eb7141ad93ae11c7339be822003eb4e7f72
Gamer-XR637 <168220319+GamerXR-637@users.noreply.github.com>
2025-06-09 13:40:00 -0400

    Add files via upload

---
+++ chrome/README.md
@@ -0,0 +1,2 @@
+'Courier New', Courier, monospace; Tiltes
+'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; Text
---
+++ chrome/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('keydown', (event) => {
+    if (event.key.toLowerCase() === 't') {
+        window.location.href = 'lol.html';
+    }
+});
+
+document.addEventListener('keydown', (event) => {
+    if (event.key.toLowerCase() === 'z') {
+        window.location.href = 'index.html';
+    }
+});
